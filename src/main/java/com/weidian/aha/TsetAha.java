34b659b8763050c6442f19ac7c4a6a13f57dcc6d
zongzi <545670755@qq.com>
2019-01-08 21:29:05 +0800

    添加了测试代码test2

---
+++ src/main/java/com/weidian/aha/TsetAha.java
@@ -0,0 +1,11 @@
+package com.weidian.aha;
+
+/**
+ * @auther ZongZi
+ * @date 2019/1/8 9:17 PM
+ */
+public class TsetAha {
+    public static void main(String[] args) {
+        System.out.println("nihao teste2");
+    }
+}
